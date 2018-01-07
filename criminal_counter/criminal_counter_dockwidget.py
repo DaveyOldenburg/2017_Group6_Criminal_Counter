9bf5f339e7aaae116dfeb566ef5d01953caa0b0c
Lan2020 <33870342+Lan2020@users.noreply.github.com>
2018-01-07 14:05:56 +0100

    new plugin

---
+++ criminal_counter/criminal_counter_dockwidget.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ criminal_counterDockWidget
+                                 A QGIS plugin
+ This plugin helps the policeman on duty to catch criminals
+                             -------------------
+        begin                : 2018-01-07
+        git sha              : $Format:%H$
+        copyright            : (C) 2018 by group6
+        email                : ^dushenglan940128@163.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from PyQt4 import QtGui, uic
+from PyQt4.QtCore import pyqtSignal
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'criminal_counter_dockwidget_base.ui'))
+
+
+class criminal_counterDockWidget(QtGui.QDockWidget, FORM_CLASS):
+
+    closingPlugin = pyqtSignal()
+
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(criminal_counterDockWidget, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
+
+    def closeEvent(self, event):
+        self.closingPlugin.emit()
+        event.accept()
+
